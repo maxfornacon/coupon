ad09e28a72dc0ddf8e4335dbc3668d86f301f3f0
unknown <dominik@MITNB728.corp.manitz-it.de>
2019-02-23 17:02:32 +0100

    test change

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Dema',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
