15d34d92c3016f8c0e4129d888367c67e6d5559d
Dominik Tilgner <kinimod1998.dt@gmail.com>
2019-03-09 11:31:08 +0100

    Added simple colorpicker in settings

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,4 +1,11 @@
+import 'dart:async';
+
 import "package:flutter/material.dart";
+
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:flutter_colorpicker/material_picker.dart';
+import 'package:flutter_colorpicker/block_picker.dart';
+import 'package:flutter_colorpicker/utils.dart';
 
 class SettingsPage extends StatefulWidget {
   @override
@@ -6,6 +13,16 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+
+  Color currentColor = const Color(0xff443a49);
+
+  void changeColor(Color color) => setState(() => currentColor = color);
+  void changeColorAndPopout(Color color) => setState(() {
+    currentColor = color;
+    Timer(const Duration(milliseconds: 500),
+            () => Navigator.of(context).pop());
+  });
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +30,30 @@ class _SettingsPageState extends State<SettingsPage> {
         title: Text("Einstellungen"),
       ),
       body: Card(
-
+        child: RaisedButton(
+          elevation: 5.0,
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text('Theme-Farbe auswählen'),
+                  content: SingleChildScrollView(
+                    child: BlockPicker(
+                      pickerColor: currentColor,
+                      onColorChanged: changeColor,
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+          child: const Text('Theme-Farbe'),
+          color: currentColor,
+          textColor: useWhiteForeground(currentColor)
+              ? const Color(0xffffffff)
+              : const Color(0xff000000),
+        ),
       ),
     );
   }
